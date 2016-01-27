0ec07549406ba875b1c090bab60841b7f5b864b4
Brandon Evans <brandon@brandonevans.ca>
2016-01-26 22:37:27 -0700

    Connect view controllers to storyboard

--- FasterDownhill/LogViewController.swift
+++ FasterDownhill/LogViewController.swift
@@ -1,14 +1,12 @@
 import UIKit
 
-class LogViewController: UIViewController {
+final class LogViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
+final class LogTableCell: UITableViewCell {
+}
--- FasterDownhill/TrackerViewController.swift
+++ FasterDownhill/TrackerViewController.swift
@@ -1,14 +1,30 @@
 import UIKit
+import MapKit
 
-class TrackerViewController: UIViewController {
+final class TrackerViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var proximitySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var locationNameTextField: UITextField!
+    @IBOutlet weak var getNameButton: UIButton!
+    @IBOutlet weak var locateButton: UIButton!
+    @IBOutlet weak var trackPointButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Actions
+
+    @IBAction func changeProximityType(sender: AnyObject) {
+    }
+
+    @IBAction func getCurrentLocation(sender: AnyObject) {
+    }
+
+    @IBAction func getNameOfCurrentLocation(sender: AnyObject) {
+    }
+
+    @IBAction func trackPoint(sender: AnyObject) {
     }
 }
 
